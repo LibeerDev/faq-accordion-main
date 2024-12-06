e0b5cf45252e5981ec4e67b9c51667b56b32fe63
LibeerDev <libeergithub@gmail.com>
2024-12-06 19:41:24 +0100

    nvm

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ cardItems.forEach(item => {
     const paragraph = item.querySelector('p');
 
     button.addEventListener('click', () => {
+
+        //reset
+        cardItems.forEach(item => {
+            const button = item.querySelector('i');
+            const paragraph = item.querySelector('p');
+
+            paragraph.classList.add('hidden');
+            button.className = "ri-add-circle-fill button-dropdown";
+        });
+
         paragraph.classList.toggle('hidden');
         button.className = "ri-indeterminate-circle-line button-dropdown";
         if (paragraph.classList.contains('hidden')) {
